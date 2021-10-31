c76606fc719d43c0508cf37cd84bd0e557af0f7b
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-11-01 00:15:07 +0500

    Updated Implementation of JsonToCsv

--- jcsv.go
+++ jcsv.go
@@ -2,24 +2,33 @@ package jcsv
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
-type Json struct {
-	Data map[string]string
+type JSOBObject struct {
+	Data map[string]map[string]string
 }
 
 func JsonToCsv(j []byte, addHeaders bool) ([]byte, error) {
 	// TODO: convert JSON data in `j` into CSV format and return
-	var JSON Json
+	var JSON JSOBObject
 	json.Unmarshal(j, &JSON.Data)
+	fmt.Println(JSON.Data)
 	var CSVData string
 	var header string
-	for key, element := range JSON.Data {
-		header = header + "," + key
-		CSVData = CSVData + "," + element
+	isHeaderFormed := false
+	for key, _ := range JSON.Data {
+		var rowData string
+		for innerKey, value := range JSON.Data[key] {
+			if !isHeaderFormed {
+				header = header + "," + innerKey
+			}
+			rowData = rowData + "," + string(value)
+		}
+		isHeaderFormed = true
+		CSVData = CSVData + rowData[1:] + "\n"
 	}
 	header = header[1:]
-	CSVData = CSVData[1:]
 	if addHeaders == true {
 		return []byte(header + "\n" + CSVData), nil
 	}
@@ -28,5 +37,6 @@ func JsonToCsv(j []byte, addHeaders bool) ([]byte, error) {
 
 func CsvToJson(c []byte, hasHeaders bool) ([]byte, error) {
 	// TODO: convert CSV data in `c` into JSON format and return
+	//if(has)
 	return nil, nil
 }
