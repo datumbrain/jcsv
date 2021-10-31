edf57738a3facfbcc86eeffe51add2921a9f8137
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-10-31 23:28:01 +0500

    Implemented the Function JsonToCsv

--- jcsv.go
+++ jcsv.go
@@ -1,8 +1,29 @@
 package jcsv
 
+import (
+	"encoding/json"
+)
+
+type Json struct {
+	Data map[string]string
+}
+
 func JsonToCsv(j []byte, addHeaders bool) ([]byte, error) {
 	// TODO: convert JSON data in `j` into CSV format and return
-	return nil, nil
+	var JSON Json
+	json.Unmarshal(j, &JSON.Data)
+	var CSVData string
+	var header string
+	for key, element := range JSON.Data {
+		header = header + "," + key
+		CSVData = CSVData + "," + element
+	}
+	header = header[1:]
+	CSVData = CSVData[1:]
+	if addHeaders == true {
+		return []byte(header + "\n" + CSVData), nil
+	}
+	return []byte(CSVData), nil
 }
 
 func CsvToJson(c []byte, hasHeaders bool) ([]byte, error) {
