af6a9c8610e2f1a2cb3d67cae0bb26d4fa535db7
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-11-29 13:07:40 +0500

    Created Convertion Functions
    * toByteArray that converts [][]string to []byte
    * toArrayOfArrayOfString that converts []byte to [][]string

--- jcsv.go
+++ jcsv.go
@@ -78,3 +78,30 @@ func CsvToJson(c []byte, hasHeaders bool) ([]byte, error) {
 
 	return []byte(jsonData), nil
 }
+
+func toByteArray(data [][]string) []byte {
+	var convertedData string
+
+	for _, strArray := range data {
+		// separate []string at every index of [][]string by "\n"
+		// separate string at every index of []string by "~"
+		// This formating will be considered while converted [][] string again to []byte
+		convertedData = convertedData + strings.Join(strArray, "~") + "\n"
+	}
+
+	return []byte(convertedData)
+}
+
+func toArrayOfArrayOfString(data []byte) [][]string {
+	var convertedData [][]string
+
+	// spliting on the basis of "\n" that will give the actual []string at every index of [][]string
+	strArray := strings.Split(string(data), "\n")
+	for _, str := range strArray {
+		// spliting on the basis of "~" that will give the actual string at every index of []string
+		convertedData = append(convertedData, strings.Split(str, "~"))
+	}
+	convertedData = convertedData[0 : len(convertedData)-1]
+
+	return convertedData
+}
